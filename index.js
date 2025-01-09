367316bb06ac38facbcc94715851879e5ee58249
Haris-Zafar <115533881+Haris-Zafar@users.noreply.github.com>
2025-01-10 02:07:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const posts = require('./posts');
-const comments = require('./comments');
-
-app.use(express.json());
+const posts = require('./routes/posts');
+const comments = require('./routes/comments');
 
 app.use('/posts', posts);
 app.use('/comments', comments);
 
-const PORT = 3000;
-app.listen(PORT, () => {
+// Only start the server if this file is run directly
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = 3000;
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+}
+
+module.exports = app;
